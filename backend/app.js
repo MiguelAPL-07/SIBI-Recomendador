347d729b3349debcb67d0e0eb03d904716efc150
MiguelAPL-07 <miguelangelpl2020@gmail.com>
2022-11-16 14:07:15 +0100

    Conexion backend con frontend

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,27 @@
 // Constante necesaria para utilizar expressJS
 const express = require('express');
-
 const app = express();
 
-const port = 3001;
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Bienvenido al recomendador de conciertos!')
+    console.log("LLEGA");
+    //res.send('Bienvenido al recomendador de conciertos!');
 });
 
+app.post('/registro', (req, res) => {
+    const {user, pass} = req.body;
+    console.log(user + " " + pass);
+    // Conectar la sesion con la base de datos
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+    // Realizar consulta a la base de datos
+
+    // Ver resultados
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
+import React, { useState } from 'react';
+import axios from "axios";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function App() {
+
+  const [details, setDetails] = useState({user:'', pass:''});
+
+  const handleSubmit = () => {
+    console.log(details.user + " " + details.pass);
+    axios.post('/registro', {
+      user:details.user, pass:details.pass
+    }).then(response => {
+      console.log("Enviado");
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Creando el recomendador!
         </p>
+        <TextField id="outlined-basic" label="User" variant="outlined"
+          onChange={e => setDetails({...details, user: e.target.value})} />
+        <TextField id="outlined-basic" label="Pass" variant="outlined" 
+          onChange={e => setDetails({...details, pass: e.target.value})} />
+        <Button onClick={handleSubmit} variant="text">Registrarse</Button>
       </header>
     </div>
   );
