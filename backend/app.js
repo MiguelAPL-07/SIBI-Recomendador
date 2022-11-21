a37526d9b07a14e76ae43ef2d3bf044666433458
MiguelAPL-07 <miguelangelpl2020@gmail.com>
2022-11-21 21:29:43 +0100

    Conexion backend con database y nodo de prueba

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,22 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json())
+// Para poder recibir datos en forma JSON del frontend
+app.use(express.json());
+
+// Base de datos
+const neo4j = require('neo4j-driver');
+
+const uri = 'neo4j+s://9d7bd182.databases.neo4j.io';
+const user = 'neo4j';
+const password = 'L3YfEDDGsRRgKIpfawlsjenGyX7bMzX_W5bPjbrQi6E';
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+const session = driver.session();
+
 
 app.get('/', (req, res) => {
     console.log("LLEGA");
-    //res.send('Bienvenido al recomendador de conciertos!');
+    res.send('Bienvenido al recomendador de conciertos!');
 });
 
 app.post('/registro', (req, res) => {
@@ -19,6 +30,19 @@ app.post('/registro', (req, res) => {
     // Ver resultados
 
 });
+
+// Ejemplo de crear un nodo
+app.post('/crearNodo', (req, res) => {
+    const {user, pass} = req.body;
+    console.log(user + " " + pass);
+    session.run(`CREATE (u:User {name: "${user}", pass: "${pass}"})`)
+        .then(resultado => {
+            res.send('Usuario registrado ' + resultado);
+    }).catch(error => {
+        console.log(error);
+    });
+});
+
 
 const PORT = process.env.PORT || 5000;
 
